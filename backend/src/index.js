b9a6925c7b126023f0582cf23d0763d720001ad4
ggang89 <ggang89@gmail.com>
2024-11-23 20:03:20 +0900

    mongoDB 가입하고 연결 => env 파일 사용

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,35 +6,45 @@ const express = require('express');
 //절대 경로를 사용하기 위해 
 const path = require('path');
 
+ // 새로운 express 어플 생성
+const app = express();
 
 const cors = require('cors');
-// Constants
-// express 서버를 위한 포트 설정, 호스트 지정
-// const PORT = 8080;
-// const HOST = "0.0.0.0";
 
-// // App
-// // 새로운 express 어플 생성
-// const app = express();
-
-// // "/"이 경로로 요청이 오면 Hellow World 결과값으로 전달
-// app.get('/', (req, res) => {
-//   res.send('Hello World');
-// });
-
-// //해당 포트와 호스트에서 HTTP 서버 시작
-// app.listen(PORT, HOST);
-// console.log(`running on http://${HOST}:${PORT}`);
-
-const app = express();
 const port = 4000;
+
+const mongoose = require('mongoose')
+
+//monDB url이 노출되면 안되므로 env파일에 넣어줌
+//env파일 사용하려면 dotenv 연결
+const dotenv = require('dotenv');
+dotenv.config();
 
 //cors문제 해결
 //미들웨어 사용시 app.use 로 접근
 app.use(cors());
 
+//express.json(): JSON 페이로드로 들어오는 구문을 파싱해준다.
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("연결 완료");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+//  "/"이 경로로 요청이 오면 Hellow World 결과값으로 전달
 app.get('/', (req, res) => {
-  res.send('ddkddkdkdk00');
+  res.send('Hello World');
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
 })
 
 //express.static(경로,파일이 있는 폴더)=>이미지, css, js파일 같은 정적 파일을 제공
@@ -42,6 +52,8 @@ app.get('/', (req, res) => {
 //절대경로 사용
 app.use(express.static(path.join(__dirname, "../uploads")));
 
+//해당 포트와 호스트에서 HTTP 서버 시작
+// app.listen(PORT, HOST);
 app.listen(port, () => {
   console.log(`${port}에서 실행되었다`)
 })
