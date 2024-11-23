976e6238477feb56c107c3986dcb6798c4b7d1cb
ggang89 <ggang89@gmail.com>
2024-11-23 18:23:07 +0900

    [add / feat] espress.static 미들웨어로 정적파일 불러오고, cors 사용 *미들웨어 사용시 app.use(사용할 미들웨어)

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,11 @@
 // express 모듈 불러오기
 const express = require('express');
 
+//절대 경로를 사용하기 위해 
+const path = require('path');
+
+
+const cors = require('cors');
 // Constants
 // express 서버를 위한 포트 설정, 호스트 지정
 // const PORT = 8080;
@@ -23,9 +28,19 @@ const express = require('express');
 
 const app = express();
 const port = 4000;
+
+//cors문제 해결
+//미들웨어 사용시 app.use 로 접근
+app.use(cors());
+
 app.get('/', (req, res) => {
   res.send('ddkddkdkdk00');
 })
+
+//express.static(경로,파일이 있는 폴더)=>이미지, css, js파일 같은 정적 파일을 제공
+//상대경로 사용 => app.use(express.static('/home', 'uploads'));
+//절대경로 사용
+app.use(express.static(path.join(__dirname, "../uploads")));
 
 app.listen(port, () => {
   console.log(`${port}에서 실행되었다`)
