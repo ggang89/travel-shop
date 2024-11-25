d68a2c3f0c6d5cf5e88be655e4149bb2ebd10477
ggang89 <ggang89@gmail.com>
2024-11-25 16:25:51 +0900

    [add/error] toastify(경고창)설치 했는데 안 뜸

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,20 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './layout/NavBar/index.jsx'
 import './App.css'
 import Footer from './layout/Footer/index.jsx'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
 
   return (
     <div className='flex flex-col h-screen justify-between'>
+
+      <ToastContainer
+        position='bottom-right'
+        theme='light'
+        pauseOnHover
+        autoClose={1500}
+      />
       <Navbar />
       <main className='mb-auto w-10/12 max-w-4xl mx-auto'>
         <Outlet />
