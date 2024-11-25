f2c48b760fc506d0b83a63f25c8af7309803d057
ggang89 <ggang89@gmail.com>
2024-11-25 14:56:46 +0900

    [add / fetch] react-hook-form으로 유효성 검사 로직 구성

--- frontend/src/pages/RegisterPage/index.jsx
+++ frontend/src/pages/RegisterPage/index.jsx
@@ -1,9 +1,31 @@
+import { useForm } from 'react-hook-form';
+
 const RegisterPage = () => {
+  const { register, handleSubmit, formState: { errors }, reset } = useForm({ mode: 'onChange' })
+  
+  const onSubmit = () => {
+    reset();
+  }
+
+  const userEmail = {
+    required:"필수 필드입니다."
+  }
+const userName = {
+  required: "필수 필드입니다.",
+};
+  const userPassword = {
+    required: "필수 필드입니다.",
+    minLength: {
+      value: 6,
+      message:"최소 6자입니다."
+    }
+  }
+
   return (
     <section className="flex flex-col justify-center mt-20 max-w-[400px] m-auto">
       <div className="p-6 bg-white rounded-md shadow-md">
         <h1 className="text-3xl font-semibold text-center">회원가입</h1>
-        <form className="mt-6">
+        <form className="mt-6" onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-2">
             <label
               htmlFor="email"
@@ -15,7 +37,13 @@ const RegisterPage = () => {
               type="email"
               id="email"
               className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+              {...register("email", userEmail)}
             />
+            {errors?.email && (
+              <div>
+                <span className="text-red-500">{errors.email.message}</span>
+              </div>
+            )}
           </div>
 
           <div className="mb-2">
@@ -29,7 +57,13 @@ const RegisterPage = () => {
               type="text"
               id="name"
               className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+              {...register("name", userName)}
             />
+            {errors?.name && (
+              <div>
+                <span className="text-red-500">{errors.name.message}</span>
+              </div>
+            )}
           </div>
 
           <div className="mb-2">
@@ -43,21 +77,33 @@ const RegisterPage = () => {
               type="password"
               id="password"
               className="w-full px-4 py-2 mt-2 bg-white border rounded-md"
+              {...register("password", userPassword)}
             />
+            {errors?.password && (
+              <div>
+                <span className="text-red-500">{errors.password.message}</span>
+              </div>
+            )}
           </div>
 
           <div className="mt-6">
-          <button type="submit" className="w-full bg-black text-white px-4 py-2 rounded-md hover:bg-gray-700 duration-200">회원가입</button>
+            <button
+              type="submit"
+              className="w-full bg-black text-white px-4 py-2 rounded-md hover:bg-gray-700 duration-200"
+            >
+              회원가입
+            </button>
           </div>
 
           <p className="mt-8 text-xs font-light text-center text-gray-500">
             아이디가 있다면?{" "}
-            <a href="/login"
-            className="font-medium hover:underline text-gray-800">
+            <a
+              href="/login"
+              className="font-medium hover:underline text-gray-800"
+            >
               로그인
             </a>
           </p>
-        
         </form>
       </div>
     </section>
