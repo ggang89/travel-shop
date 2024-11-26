1a843e2a71df4b4e6f1ba11c876beb507faa7bd2
ggang89 <ggang89@gmail.com>
2024-11-26 23:34:42 +0900

    회원가입시 암호화된 비밀번호 적용하기 => salt와 hash 사용

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -23,6 +23,25 @@ const userSchema = mongoose.Schema({
   }
 });
 
+// 꼭 모델위에 작성해줘야 함.모델 전에 동작되어야 하므로
+// pre메소드 => 유저데이터를 몽고디비에 저장하기 전에 콜백함수 호출함
+
+userSchema.pre('save', async function (next) {
+  let user = this; //user 데이터
+
+  //만약 패스워드를 수정하면
+  if (user.isModified('password')) {
+    //salt와 hash로 암호화함
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+
+    //hash된 password로 넣어줌
+    user.password = hash;
+  }
+
+  next();
+})
+
 const User = mongoose.model("User", userSchema);
 
 //다른 모듈에서 사용할 수 있도록
