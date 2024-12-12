a7da26c3ffdcde5ff89b9b738fa7cd177af27b97
ggang89 <ggang89@gmail.com>
2024-12-12 15:49:38 +0900

    fix : error 수정 => map 괄호 문제

--- frontend/src/components/FileUpload.jsx
+++ frontend/src/components/FileUpload.jsx
@@ -7,8 +7,9 @@ const FileUpload = ({ images, onImageChange }) => {
     let formData = new FormData();
 
     const config = {
-      header: { "content-type": "multipart/form-data" },
-    };
+      header: { "content-type": "multipart/form-data" }
+    }
+
     formData.append("file", files[0]);
 
     try {
@@ -44,15 +45,15 @@ const FileUpload = ({ images, onImageChange }) => {
       </Dropzone>
 
       <div className="flex-grow h-[300px] border flex items-center justify-center overflow-x-scroll overflow-y-hidden">
-        {images.map((image) => {
+        {images.map((image) => (
           <div key={image} onClick={() => handleDelete(image)}>
             <img
               className="min-w-[300px] h-[300px]"
               src={`${import.meta.env.VITE_SERVER_URL}/${image}`}
               alt={image}
             />
-          </div>;
-        })}
+          </div>
+        ))}
       </div>
     </div>
   );
