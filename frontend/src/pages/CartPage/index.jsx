d69002e799582384f7c68c3073772aa3308a030e
ggang89 <ggang89@gmail.com>
2024-12-19 19:05:55 +0900

    쇼핑카트 페이지 기본 UI 생성 + cartDetail에 정보 가져와서 변수에 저장

--- frontend/src/pages/CartPage/index.jsx
+++ frontend/src/pages/CartPage/index.jsx
@@ -1,31 +1,61 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getCartItems } from '../../store/thunkFunctions';
-
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCartItems } from "../../store/thunkFunctions";
 
 const CartPage = () => {
-  const userData = useSelector(state => state.user?.userData);
+  const userData = useSelector((state) => state.user?.userData);
+  const cartDetail = useSelector((state) => state.user?.cartDetail);
   const dispatch = useDispatch();
+  const [total, setTotal] = useState(0);
 
   useEffect(() => {
     let cartItemIds = [];
-    
-    if (userData?.cart && userData.cart.length  > 0) {
-      userData.cart.forEach(item => {
+
+    if (userData?.cart && userData.cart.length > 0) {
+      userData.cart.forEach((item) => {
         cartItemIds.push(item.id);
-      })
+      });
       const body = {
         cartItemIds,
-        userCart:userData.cart
-      }
+        userCart: userData.cart,
+      };
       dispatch(getCartItems(body));
     }
-  }, [dispatch, userData])
-  
+  }, [dispatch, userData]);
+
+  useEffect(() => {
+    calculateTotal(cartDetail);
+  }, [cartDetail]);
+
+  const calculateTotal = (cartItems) => {
+    let total = 0;
+    cartItems.map((item) => (total += item.price * item.quantity));
+    setTotal(total);
+  };
 
   return (
-    <div>cartPage</div>
-  )
-}
+    <section>
+      <div className="text-center m-7">
+        <h2 className="text-2xl">나의 장바구니</h2>
+      </div>
 
-export default CartPage
+      {cartDetail?.length > 0 ? (
+        <>
+          <div className="mt-10">
+            <p>
+              <span className="font-bold">합계 : </span>
+              {total}원
+            </p>
+            <button className="text-white bg-black rounded-md hover:bg-gray-500 px-4 py-2 mt-5">
+              결제하기
+            </button>
+          </div>
+        </>
+      ) : (
+        <p>장바구니가 비었습니다.</p>
+      )}
+    </section>
+  );
+};
+
+export default CartPage;
